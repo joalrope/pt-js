4c42d2da5b2bf4859b73f718d08fe943859ac3c7
joalrope <joalrope@gmail.com>
2022-05-02 15:07:06 -0400

    navbar 0205 refact

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,13 +1,28 @@
 $(function () {
+  $('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
   $('main').on('scroll', function (e) {
     const scroll = $('main').scrollTop();
 
     if (scroll >= 70) {
-      $('.navbar').addClass('bg-primary text-white');
+      $('.navbar').addClass('bg-primary');
       $('.nav-link').addClass('text-white');
     } else {
-      $('.navbar').removeClass('bg-primary text-white');
-      $('.nav-link').removeClass('text-white');
+      $('.navbar').removeClass('bg-primary');
+      if (window.innerWidth > 576) {
+        $('.nav-link').removeClass('text-white');
+      }
+    }
+  });
+
+  $(window).resize(function () {
+    if (window.innerWidth < 576) {
+      $('.nav-pills').addClass('bg-primary');
+      $('.nav-link').addClass('text-white');
+    } else {
+      $('.nav-pills').removeClass('bg-primary');
     }
   });
 });
